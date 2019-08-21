1f3a6d0dcf699114646749b2fa6182a0cc208858
root <root@localhost.localdomain>
2019-08-21 06:56:22 -0400

    central ecu socket TCP -> UDP + Hum int sinchro

--- centralecu.c
+++ centralecu.c
@@ -11,6 +11,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h> /* For AFINET sockets */
 #define DEFAULT_PROTOCOL 0
+#define HUMAN_INTERFACE_PORT 1025
+#define UDP_CENECU_PORT 1026
+#define MAXLINE 1024
 
 int readLine(int fd, char*str) {
     int n; 
@@ -21,60 +24,99 @@ int readLine(int fd, char*str) {
 }
 
 
+void createSocket(int* serverFd, struct sockaddr* clientSockAddrPtr, int* clientLen, int port){
+    int serverLen;
+    struct sockaddr_in serverINETAddress;/*Server address*/
+    struct sockaddr_in clientINETAddress;/*Client address*/
+    struct sockaddr* serverSockAddrPtr;/*Ptr to server*/
+    /* Ignore death-of-child signals to prevent zombies */
+    signal (SIGCHLD, SIG_IGN);
+    serverSockAddrPtr=(struct sockaddr*) &serverINETAddress;
+    serverLen = sizeof(serverINETAddress);
+    clientSockAddrPtr=(struct sockaddr*) &clientINETAddress;
+    *clientLen=sizeof (clientINETAddress);/*Create a INET socket, bidirectional, default protocol */
+    *serverFd=socket (AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);
+    serverINETAddress.sin_family = AF_INET;
+    serverINETAddress.sin_port = htons (port);
+    serverINETAddress.sin_addr.s_addr=htonl(INADDR_LOOPBACK);
+    bind(*serverFd, serverSockAddrPtr, serverLen);
+    listen (*serverFd, 2);
+}
+
+void createUDPSocket(int* serverFd, int port){
+    int serverLen;
+    struct sockaddr_in serverINETAddress;/*Server address*/
+   // struct sockaddr_in clientINETAddress;/*Client address*/
+    struct sockaddr* serverSockAddrPtr;/*Ptr to server*/
+    /* Ignore death-of-child signals to prevent zombies */
+    signal (SIGCHLD, SIG_IGN);
+    *serverFd=socket (AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
+
+    memset(&serverINETAddress, 0 , sizeof(serverINETAddress));
+    serverSockAddrPtr=(struct sockaddr*) &serverINETAddress;
+    serverLen = sizeof(serverINETAddress);
+    //clientSockAddrPtr=(struct sockaddr*) &clientINETAddress;
+   // *clientLen=sizeof (clientINETAddress);/*Create a INET socket, bidirectional, default protocol */
+    
+    serverINETAddress.sin_family = AF_INET;
+    serverINETAddress.sin_port = htons (port);
+    serverINETAddress.sin_addr.s_addr= INADDR_ANY;
+    bind(*serverFd, serverSockAddrPtr, serverLen);
+}
+
+
 
 int main(){
 
-int serverFd, clientFd, serverLen, clientLen;
-unsigned short int port = 1025;
-struct sockaddr_in serverINETAddress;/*Server address*/
-struct sockaddr_in clientINETAddress;/*Client address*/
-struct sockaddr* serverSockAddrPtr;/*Ptr to server*/
-struct sockaddr* clientSockAddrPtr; /*Ptr to client
-/* Ignore death-of-child signals to prevent zombies */
-signal (SIGCHLD, SIG_IGN);
-serverSockAddrPtr=(struct sockaddr*) &serverINETAddress;
-serverLen = sizeof(serverINETAddress);
-clientSockAddrPtr=(struct sockaddr*) &clientINETAddress;
-clientLen=sizeof (clientINETAddress);/*Create a INET socket, bidirectional, default protocol */
-serverFd=socket (AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);
-serverINETAddress.sin_family = AF_INET;
-serverINETAddress.sin_port = htons (port);
-serverINETAddress.sin_addr.s_addr=htonl(INADDR_LOOPBACK);
-bind(serverFd, serverSockAddrPtr, serverLen);
-listen (serverFd, 2);
-pid_t pid; 
-int i = 0;
-int status;
-
-    pid = fork();
-
-    if(pid == 0 )
-    {
-        printf("sono il client");
-        execl("/usr/bin/xterm", "xterm", "./humaninterface", NULL);
-        exit(1);
+/*int cen_ecu_Fd, hum_int_len, hum_int_Fd, hum_int_status;
+struct sockaddr* hum_int_SockAddrPtr;
+pid_t hum_int_pid;
+createSocket(&cen_ecu_Fd, hum_int_SockAddrPtr, &hum_int_len, HUMAN_INTERFACE_PORT); //Genereting socket for human interface
+hum_int_pid = fork();
+if(hum_int_pid == 0 )
+{
+    execl("/usr/bin/xterm", "xterm", "./humaninterface", NULL); // execute human interface in a focked process on a new terminal
+    exit(1);
+}
+else {
+    printf("waiting for the human interface...\n");
+    hum_int_Fd=accept(cen_ecu_Fd,hum_int_SockAddrPtr,&hum_int_len); //waiting the connection of the human interface generated
+    printf("human interface connected\n %d", hum_int_Fd);
+    char str[200];
+    while(readLine(hum_int_Fd, str) && strcmp(str,"FINE") != 0){
+        printf("%s\n", str);
     }
-    else {
-        printf("sono il server");
-        printf("waiting for the client\n");
-        clientFd=accept(serverFd,clientSockAddrPtr,&clientLen);
+    waitpid(hum_int_pid, &hum_int_status, 0);
 
-        printf("connected\n %d", clientFd);
-            char str[200];
-            while(readLine(clientFd, str) && strcmp(str,"$") != 0){
-                printf("%s\n", str);
-            }
-            printf("\n fine zebra");
-            close (clientFd); /* Close the socket */
-            exit (/* EXIT_SUCCESS */ 0); /* Terminate */
-
-    }
-
-   
+    printf("\nFINE %d", hum_int_status);
+    close (hum_int_Fd); // Close the socket 
+    exit (0); // Terminate 
 
 
+}*/
 
+int cen_ecu_sockFd, hum_int_len, hum_int_Fd;
+struct sockaddr* clientAddr;
+pid_t hum_int_pid;
+char buffer[MAXLINE];
 
+createUDPSocket(&cen_ecu_sockFd, UDP_CENECU_PORT); //Genereting socket for human interface
+hum_int_pid = fork();
+if(hum_int_pid == 0 )
+{
+    execl("/usr/bin/xterm", "xterm", "./humaninterface", NULL); // execute human interface in a focked process on a new terminal
+    exit(1);
+}
+else {
+    
+    do{
+        int n, len;
+        printf("Waiting for a message...\n");
+        n = recvfrom(cen_ecu_sockFd,(char *)buffer, MAXLINE, MSG_WAITALL, clientAddr, &len);
+        buffer[n] = '\0';
+        printf("Client %s\n", buffer);
+    }while(strcmp(buffer,"FINE") != 0);
+}
 return 1;
 }
 
--- humaninterface.c
+++ humaninterface.c
@@ -5,49 +5,114 @@
 #include <sys/socket.h>
 #include <netinet/in.h> /* For AF_INET sockets */
 #define DEFAULT_PROTOCOL 0
+#define HUMAN_INTERFACE_PORT 1025
+#define MAXLINE 1024
 
+
+void createUDPSocket(int* serverFd, int port){
+    int serverLen;
+    struct sockaddr_in serverINETAddress;/*Server address*/
+   // struct sockaddr_in clientINETAddress;/*Client address*/
+    struct sockaddr* serverSockAddrPtr;/*Ptr to server*/
+    /* Ignore death-of-child signals to prevent zombies */
+    signal (SIGCHLD, SIG_IGN);
+    *serverFd=socket (AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
+
+    memset(&serverINETAddress, 0 , sizeof(serverINETAddress));
+    serverSockAddrPtr=(struct sockaddr*) &serverINETAddress;
+    serverLen = sizeof(serverINETAddress);
+    //clientSockAddrPtr=(struct sockaddr*) &clientINETAddress;
+   // *clientLen=sizeof (clientINETAddress);/*Create a INET socket, bidirectional, default protocol */
+    
+    serverINETAddress.sin_family = AF_INET;
+    serverINETAddress.sin_port = htons (port);
+    serverINETAddress.sin_addr.s_addr= INADDR_ANY;
+    bind(*serverFd, serverSockAddrPtr, serverLen);
+}
 
 int main (void) {
 
-	sleep(1);
-	int clientFd, serverLen, result;
-	unsigned short int port = 1025;
-	struct sockaddr_in serverINETAddress;
-	struct sockaddr* serverSockAddrPtr;
-	serverSockAddrPtr=(struct sockaddr*)
-	&serverINETAddress;
-	serverLen = sizeof (serverINETAddress);
-	clientFd=socket(AF_INET,SOCK_STREAM,DEFAULT_PROTOCOL);
-	serverINETAddress.sin_family = AF_INET;
-	serverINETAddress.sin_port = htons (port);
-	serverINETAddress.sin_addr.s_addr = htonl
+/*	sleep(1); //delay to wait central ecu to open the socket
+	int hum_int_Fd, cen_ecu_Len, result;
+	struct sockaddr_in cen_ecu_INETAddress;
+	struct sockaddr*  cen_ecu_SockAddrPtr;
+	cen_ecu_SockAddrPtr=(struct sockaddr*)
+	&cen_ecu_INETAddress;
+	cen_ecu_Len = sizeof (cen_ecu_INETAddress);
+	hum_int_Fd=socket(AF_INET,SOCK_STREAM,DEFAULT_PROTOCOL);
+	cen_ecu_INETAddress.sin_family = AF_INET;
+	cen_ecu_INETAddress.sin_port = htons (HUMAN_INTERFACE_PORT);
+	cen_ecu_INETAddress.sin_addr.s_addr = htonl
 	(INADDR_LOOPBACK);
+	pid_t output_pid;
 	do {
-/* Loop until a connection is made with the server */
-		result=connect(clientFd, serverSockAddrPtr,serverLen);
+//Loop until a connection is made with the server 
+		result=connect(hum_int_Fd,  cen_ecu_SockAddrPtr,cen_ecu_Len);
 
 		if (result == -1) {
 			printf("connessione fallita");
-			sleep (1);/* Wait, try again */
+			sleep (1);// Wait, try again 
 		}
 	} while (result == -1);
 	printf("Connected successesfully to Central Ecu\n");
 	printf("Write anything: \n");
 
 	char str[100];
-	int pid = fork();
-	printf("pid %d", pid);
+	output_pid = fork();
+	printf("output_pid %d", output_pid);
 	
-	if( pid != 0){
+	if( output_pid != 0){
 		do {
 			scanf("%s", &str);
-			write(clientFd, str, strlen(str)+1);
-		}while( strcmp(str,"$") != 0 );
+			write(hum_int_Fd, str, strlen(str)+1);
+		}while( strcmp(str,"FINE") != 0 );
 
-		close (clientFd); /* Close the socket */
-		exit (/* EXIT_SUCCESS */ 0); /* Done */
+		printf('killing');
+		sleep(3);
+		close (hum_int_Fd); // Close the socket 
+		exit ( 0); // Done 
 	}
 	else {
-		execlp("/usr/bin/xterm","xterm", "-e","tail -f  ./centralecu.log",0);
+		execl("/usr/bin/xterm","xterm", "-e","tail -f  ./centralecu.log",0);
+	} */
+
+	int hum_int_sockFd;
+	struct sockaddr_in cen_ecu_addr;
+	pid_t hum_int_pid;
+	char buffer[MAXLINE];
+	char* hello = "hello diocane";
+	pid_t output_pid;
+
+	sleep(1);
+	hum_int_sockFd = socket(AF_INET, SOCK_DGRAM, 0);
+	memset(&cen_ecu_addr, 0 , sizeof(cen_ecu_addr));
+	cen_ecu_addr.sin_family = AF_INET;
+    cen_ecu_addr.sin_port = htons (1026);
+    cen_ecu_addr.sin_addr.s_addr= INADDR_ANY;
+
+
+	char str[100];
+	output_pid = fork();
+	printf("output_pid %d", output_pid);
+	
+	if( output_pid != 0){
+		do {
+			scanf("%s", &str);
+			sendto(hum_int_sockFd, (const char *) str, strlen(str), MSG_CONFIRM,
+    		(const struct sockaddr *) &cen_ecu_addr, sizeof(cen_ecu_addr));
+		}while( strcmp(str,"FINE") != 0 );
+
+		kill(output_pid, SIGKILL);
+		close (hum_int_sockFd); // Close the socket 
+		exit ( 0); // Done 
+	}
+	else {
+		execl("/usr/bin/xterm","xterm", "-e","tail -f  ./centralecu.log",0);
 	} 
+
+    
+
+    close(hum_int_sockFd);
+    return 0;
+
 }
