d05b32ce8235f8258557ceddc1d0ca00e064e49d
root <root@localhost.localdomain>
2019-09-09 12:38:40 -0200

    caccola

--- centralecu.c
+++ centralecu.c
@@ -11,16 +11,15 @@
 #include <sys/socket.h>
 #include <netinet/in.h> /* For AFINET sockets */
 #include <malloc.h>
-#define DEFAULT_PROTOCOL 0
-#define FWC_INPUT '0'
-#define HUMAN_INTERFACE_PORT 1025
-#define UDP_CENECU_PORT 1026 //Default UDP socket port for central ECU
-#define MAXLINE 1024
+#include "constants.h"
+
+int tc_sock_pair[2]; //TCP socket descriptors to communicate with throttle
+int bbw_sock_pair[2]; //TCP socked descriptors to communicate with break by wire
 
 int readLine(int fd, char*str) {
     int n; 
     do {
-        n = read (fd,str,1);
+        n = read(fd,str,1);
     } while ( n> 0 && *str++ != '\0');
     return (n>0);
 }
@@ -89,10 +88,6 @@ void runThrottleControl(int fd, int* speed){
     exit(0);
 }
 
-void handleHumanInputs(){
-
-}
-
 void sendToCenEcu(int fd, char str[MAXLINE]){
     struct sockaddr_in cen_ecu_addr;
     memset(&cen_ecu_addr, 0 , sizeof(cen_ecu_addr));
@@ -113,7 +108,7 @@ void runFrontWindshieldCamera(){
     int speedLimit;
     char line[MAXLINE];
     char output[MAXLINE];
-    output[0] = FWC_INPUT;
+    output[0] = FRONT_CAMERA_CODE;
 
     fc_sockFd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -125,6 +120,23 @@ void runFrontWindshieldCamera(){
     }
     fclose(fptr);
     close(fc_sockFd);
+    exit(0);
+}
+
+void runBrakeByWire(int fd){
+    char str[MAXLINE];
+    while(1){
+        int n = 0;
+        n = read(fd, str, 1);
+        if(n != 0){
+            readLine(fd, str+1);
+            printf("COMANDO: %s", str);
+        }
+        else{
+            printf("NO ACTION");
+        }
+        sleep(1);
+    }
     exit(0);
 }
 
@@ -145,22 +157,32 @@ char* substring(char *src, int from){
     }
     dest -= len;
     return dest;
-
 }
 
-void handleSensorInput(char input[MAXLINE]){
-
+/*void handleSensorInput(char input[MAXLINE]){
     char code;
     code = input[0];
     char *command;
     command = substring(input, 1);
-
+    switch(code){
+        case FRONT_CAMERA_CODE:
+            if(command[0] > '0' && command[0] < '9'){ //There is a number to process
+                int newSpeed = atoi(command);
+                if(newSpeed < speed){
+                    write(bbw_sock_pair[0], command, strlen(command) + 1);
+                }
+            }else{ // We have a string to process
+                printf("command %s", command);
+            }
+            break;
+        default: printf("Unknown command.\n");
+    }
 
     printf("command code: %c\n", code);
 
     printf("command %s\n", command);
 }
-
+*/
 
 
 
@@ -200,13 +222,15 @@ int main(){
     pid_t fwc_pid; //PID front wide camera process
     pid_t tc_pid; //PID throttle control process
     pid_t hum_int_pid; //PID for human interface
-    char buffer[MAXLINE]; //Buffer to store UDP messages
-    int thr_sock_pair[2]; //TCP socket descriptors to communicate with throttle
+    pid_t bbw_pid; //PID for break by wire
     int speed = 0; //Car speed
+    char buffer[MAXLINE]; //Buffer to store UDP messages
+    int started = 0; //Boolean variable to tell if process has received INIZIO
 
     createUDPSocket(&cen_ecu_sockUDPFd, UDP_CENECU_PORT); //Generating socket for human interface
-    socketpair(AF_UNIX, SOCK_STREAM, 0, thr_sock_pair); //Handshaking of TCP sockets now in thr_sock_pair i have 2 connected sockets
-    //  = fork();
+    socketpair(AF_UNIX, SOCK_STREAM, 0, tc_sock_pair); //Handshaking of TCP sockets now in tc_sock_pair i have 2 connected sockets
+    socketpair(AF_UNIX, SOCK_STREAM, 0, bbw_sock_pair);
+    
     printf("Processo padre %d\n", getpid());
     if((hum_int_pid = fork()) == 0){ //I'm the child humaninterface
         execl("/usr/bin/xterm", "xterm", "./humaninterface", NULL); // execute human interface in a forked process on a new terminal
@@ -214,23 +238,62 @@ int main(){
     }
     else if((tc_pid = fork()) == 0){
         printf("  trottol da %d\n", getpid());
-        close(thr_sock_pair[0]); //I'm the child and i close father socket
-        runThrottleControl(thr_sock_pair[1], &speed); //funzione per controllare la velocita TODO
+        close(tc_sock_pair[0]); //I'm the child and i close father socket
+        runThrottleControl(tc_sock_pair[1], &speed); //funzione per controllare la velocita TODO
     }
     else if( (fwc_pid = fork()) == 0 ) {
         printf("  camera da %d\n", getpid());
         runFrontWindshieldCamera();
     }
+    else if( (bbw_pid = fork()) == 0){
+        printf("Break by wire %d\n", getpid());
+        close(bbw_sock_pair[0]); 
+        runBrakeByWire(bbw_sock_pair[1]);
+    }
     else {//In this else i'm the father
-        close(thr_sock_pair[1]); //I'm the father and i close child socket
+        close(tc_sock_pair[1]); //I'm the father and i close child socket
+        close(bbw_sock_pair[1]);
         do{
             int n, len;
             n = recvfrom(cen_ecu_sockUDPFd,(char *)buffer, MAXLINE, MSG_WAITALL, clientAddr, &len);
             buffer[n] = '\0';
-            if(strcmp(buffer,"FINE") != 0){
-                handleSensorInput(buffer);
-                //write(thr_sock_pair[0], buffer, strlen(buffer)+1);
-                logOutput("centralecu.log", buffer);
+            printf("partito %s", buffer);
+            if(!started){
+                if(strcmp(buffer,"INIZIO") == 0){
+                    started = 1;
+
+                }
+
+            }
+            else{
+                if(strcmp(buffer,"FINE") != 0){
+                    //handleSensorInput(buffer);
+                    //write(tc_sock_pair[0], buffer, strlen(buffer)+1);
+                    char code;
+                    code = buffer[0];
+                    char *command;
+                    command = substring(buffer, 1);
+                    printf("LISTENING FOR THINGS");
+                    switch(code){
+                        case FRONT_CAMERA_CODE:
+
+                            if(command[0] > '0' && command[0] < '9'){ //There is a number to process
+                                int newSpeed = atoi(command);
+                                if(newSpeed < speed){
+                                    write(bbw_sock_pair[0], command, strlen(command) + 1);
+                                }
+                            }else{ // We have a string to process
+                                printf("command %s", command);
+                            }
+                            break;
+                        default: printf("Unknown command. %s\n",command);
+                    }
+
+                    printf("command code: %c\n", code);
+
+                    printf("command %s\n", command);
+                    logOutput("centralecu.log", buffer);
+                }
             }
         }while(strcmp(buffer,"FINE") != 0);
     }
@@ -238,6 +301,7 @@ int main(){
 
     kill(fwc_pid, SIGKILL);
     kill(tc_pid, SIGKILL);
+    kill(bbw_pid, SIGKILL);
     printf("FINE\n");
     exit(0);
     return 0;
---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+#define FRONT_CAMERA_CODE '0'
+
+static const int DEFAULT_PROTOCOL = 0;
+static const int HUMAN_INTERFACE_PORT = 1025;
+static const int UDP_CENECU_PORT = 1026;
+static const int MAXLINE = 1024;
+#endif
