42ba45b45ae6eaba1cceb4c3315632c49b4b3893
alessandrogasparri <alessandro.gasparri97@gmail.com>
2019-09-17 14:33:07 +0200

    Removed unused files

--- actuators.h
+++
@@ -1,7 +0,0 @@
-extern void runBrakeByWire(int);
-
-extern void runThrottleControl(int);
-
-extern void runSteerByWire(int);
-
-extern void runParkAssist(int);
--- centralecu.h
+++
@@ -1,9 +0,0 @@
-extern char randomFile[50];
-
-extern char urandomFile[50];
-
-extern int updatingSpeed;
-
-extern int dangerDetected;
-
-extern void sig_handler(int);
